ad95fabf4297f5466f6a7033fa926928e839eafe
eduardodiasm <carloseduardodiasm@gmail.com>
2021-01-14 22:34:38 -0300

    problem: uri 1244 formatando o arquivo

--- problems/uri/1244/main.cpp
+++ problems/uri/1244/main.cpp
@@ -2,58 +2,62 @@
 
 using namespace std;
 
-vector <string> split_string(string text) {
+vector<string> split_string(string text)
+{
 
   int length = text.length();
 
-  vector <string> words;
+  vector<string> words;
 
-  for (size_t i = 0; i < length; i++) {
-    
+  for (size_t i = 0; i < length; i++)
+  {
+
     string word = "";
-    while (!isspace(text[i]) && i < length) {
+    while (!isspace(text[i]) && i < length)
+    {
       word += text[i++];
     }
 
-    if (word != "") {
+    if (word != "")
+    {
       words.push_back(word);
     }
-
   }
-  
-  return words;
 
+  return words;
 }
 
-int main(int argc, char const *argv[]) {
-  
+int main(int argc, char const *argv[])
+{
+
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
   int n;
-	cin >> n;
+  cin >> n;
 
   cin.ignore();
 
-	while(n--) {
+  while (n--)
+  {
 
     string text;
     getline(cin, text);
 
-    vector <string>  words = split_string(text);
+    vector<string> words = split_string(text);
 
     stable_sort(
-      words.begin(), words.end(), 
-      []( 
-        const std::string &s1, const std::string &s2) {
-          return s1.size() > s2.size(); 
-       }
-      );
+        words.begin(), words.end(),
+        [](
+            const std::string &s1, const std::string &s2) {
+          return s1.size() > s2.size();
+        });
 
     string last_element = words.back();
     words.pop_back();
 
-    for (auto word : words) {
+    for (auto word : words)
+    {
       cout << word << " ";
     }
 
